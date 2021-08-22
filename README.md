d3f2b08ec6a6be52bfd0b22a5ae8d2e24c8ffa3b
Chan Jun Shern <chanjunshern@gmail.com>
2021-08-23 02:04:55 +0800

    Use caching for speedup, apply some layouts

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # minerl-trajectory-viewer
+
+```bash
+conda activate minerl
+streamlit run main.py -- --data-dir $MINERL_DATA_ROOT
+```
--- main.py
+++ main.py
@@ -6,39 +6,75 @@ import random
 import streamlit as st
 from pathlib import Path
 
-def run_app(data_dir):
-    st.title('MineRL Trajectory Viewer')
+import time
 
-    # Select trajectory
-    data_dir = Path(data_dir)
+@st.cache(suppress_st_warning=True, allow_output_mutation=True, max_entries=1)
+def get_trajectory_names(data_dir: Path):
+    st.warning("Cache miss: `get_trajectory_names` ran")
     traj_dirs = sorted([x for x in data_dir.glob("*/*") if x.is_dir()])
     traj_names = [str(Path(x.parent.stem) / x.stem) for x in traj_dirs]
-    option = st.selectbox(
-        'Select a trajectory:',
-        traj_names)
-    chosen_path = data_dir / option
+    return traj_names
 
-    env_name = str(Path(chosen_path).parent.stem)
-    stream_name = Path(chosen_path).stem
+@st.cache(suppress_st_warning=True, allow_output_mutation=True, max_entries=1)
+def load_data(data_dir, env_name, stream_name):
+    st.warning("Cache miss: `load_data` ran")
     minerl_data = minerl.data.make(env_name, data_dir=data_dir)
-
     data_frames = list(minerl_data.load_data(stream_name, include_metadata=True))
-    
-    # Display GIF / video
-    # st.write("## Playback")
-    # st.image(str(Path(chosen_path) / "recording.mp4"))
+    return data_frames
 
-    # Frame-by-frame analysis
-    st.write("## Frame-by-frame analysis")
-    max_frame = len(data_frames) - 1
-    frame_idx = st.slider("Select frame:", 0, max_frame, 0)
+def run_app(data_dir):
+    st.set_page_config(page_title="MineRL Trajectory Viewer", page_icon=None, layout='wide')
+    st.title('MineRL Trajectory Viewer')
 
-    img = data_frames[frame_idx][0]["pov"]
-    st.image(img)
+    # col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns([2,1,1])
 
-    meta = data_frames[frame_idx][-1]
-    st.write("Metadata:", meta)
-    st.write(data_frames[frame_idx])
+    with col1:
+        # Select trajectory
+        data_dir = Path(data_dir)
+        st.write(f"Root data directory: `{data_dir}`")
+        traj_names = get_trajectory_names(data_dir)
+        option = st.selectbox(
+            'Select a trajectory:',
+            traj_names)
+        chosen_path = data_dir / option
+        st.write(f"`{option}`")
+
+        env_name = str(Path(chosen_path).parent.stem)
+        stream_name = Path(chosen_path).stem
+        data_frames = load_data(data_dir, env_name, stream_name)
+
+        # Display GIF / video
+        # st.write("## Playback")
+        # st.image(str(Path(chosen_path) / "recording.mp4"))
+
+        # Frame-by-frame analysis
+        st.write("## Frame-by-frame analysis")
+        max_frame = len(data_frames) - 1
+        frame_idx = st.slider("Select frame:", 0, max_frame, 0)
+        current_frame = data_frames[frame_idx]
+
+        img = current_frame[0]["pov"]
+        st.image(img, width=300)
+
+    meta = current_frame[-1]
+    with col2:
+        st.write("### Metadata")
+        st.write(meta)
+        st.write("### Actions")
+        st.write(current_frame[1])
+        st.write("### Episode Reward")
+        st.metric("Reward", float(current_frame[2]))
+
+    with col3:
+        st.write("### Equipped")
+        st.write(current_frame[0]["equipped_items"])
+
+        st.write("### Inventory")
+        st.write(current_frame[0]["inventory"])
+
+        st.write("### Frame contents:")
+        st.write(current_frame)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Visualize MineRL trajectories')
