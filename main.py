1f830f9253b0eab157a2ee4e30cdbb5c37aaa3ce
Chan Jun Shern <chanjunshern@gmail.com>
2021-08-22 19:01:43 +0800

    Loading data frames from data dir works

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
-import random
+import argparse
+import cv2
 import minerl
-from minerl.viewer import get_parser
+import numpy as np
+import random
+import streamlit as st
+from pathlib import Path
+
+def run_app(data_dir):
+    st.title('MineRL Trajectory Viewer')
+
+    # Select trajectory
+    data_dir = Path(data_dir)
+    traj_dirs = sorted([x for x in data_dir.glob("*/*") if x.is_dir()])
+    traj_names = [str(Path(x.parent.stem) / x.stem) for x in traj_dirs]
+    option = st.selectbox(
+        'Select a trajectory:',
+        traj_names)
+    chosen_path = data_dir / option
+
+    env_name = str(Path(chosen_path).parent.stem)
+    stream_name = Path(chosen_path).stem
+    minerl_data = minerl.data.make(env_name, data_dir=data_dir)
+
+    data_frames = list(minerl_data.load_data(stream_name, include_metadata=True))
+    
+    # Display GIF / video
+    # st.write("## Playback")
+    # st.image(str(Path(chosen_path) / "recording.mp4"))
+
+    # Frame-by-frame analysis
+    st.write("## Frame-by-frame analysis")
+    max_frame = len(data_frames) - 1
+    frame_idx = st.slider("Select frame:", 0, max_frame, 0)
+
+    img = data_frames[frame_idx][0]["pov"]
+    st.image(img)
+
+    meta = data_frames[frame_idx][-1]
+    st.write("Metadata:", meta)
+    st.write(data_frames[frame_idx])
 
 if __name__ == '__main__':
-    # args = get_parser().parse_args()
-    main(env_name="", stream_name="")
     parser = argparse.ArgumentParser(description='Visualize MineRL trajectories')
-    parser.add_argument('--env-name', required=True,
-                        help="MineRL environment, e.g. MineRLBasaltMakeWaterfall-v0")
-    parser.add_argument('--sort', choices=sort_order_choices, default='up',
-                        help='Animal sort order (default: %(default)s)')
-    parser.add_argument('--uppercase', action='store_true',
-                        help='Trajectory name, e.g. v3_accomplished_pattypan_squash_ghost-4_244-3238')
+    parser.add_argument("-d", "--data-dir", required=True,
+                        help="Root directory containing trajectory data. Default: %(default)s")
+    options = parser.parse_args()
 
-
-    print("Welcome to the MineRL Stream viewer! \n")
-
-    print(f"Building data pipeline for {env_name}")
-    data = minerl.data.make(env_name)
-
-    print(f"Loading data for {stream_name}...")
-    data_frames = list(data.load_data(stream_name, include_metadata=True))
-    meta = data_frames[0][-1]
-    # print("Data loading complete!")
-    # print(f"META DATA: {meta}")
-    # for frame in data_frames[:1]:
-    #     print(frame)
-    print(data_frames[0])
+    run_app(options.data_dir)
